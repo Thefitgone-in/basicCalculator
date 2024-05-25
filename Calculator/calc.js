861153bba3ca181f4498ded310691c1e14a9d340
Uday Mishra <110932645+Thefitgone-in@users.noreply.github.com>
2024-05-25 16:05:34 +0530

    Add files via upload

---
+++ Calculator/calc.js
@@ -0,0 +1,27 @@
+let str="";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '='){
+            str = eval(str);
+            document.querySelector('input').value = str;
+        }
+        else if(e.target.innerHTML == 'C'){
+          str = "";
+          document.querySelector('input').value = str;
+        }
+        else if(e.target.innerHTML=='M+'){
+           str = parseInt(str) + 1;
+           document.querySelector('input').value = str;
+        }
+        else if(e.target.innerHTML=='M-'){
+            str = parseInt(str) - 1;
+            document.querySelector('input').value = str;
+         }
+        else{
+            console.log(e.target.innerHTML);
+            str = str + e.target.innerHTML;
+            document.querySelector('input').value = str;
+        }
+    })
+})
